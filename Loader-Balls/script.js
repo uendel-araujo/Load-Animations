44c495ef0a7c3805d117cb40e1e98ed36f39d09e
Uendel Araujo <dendellaraujo@gmail.com>
2020-09-29 14:20:51 -0300

    :checkered_flag: Project Completion

--- Loader-Balls/script.js
+++ Loader-Balls/script.js
@@ -1,2 +1,13 @@
 const balls = document.querySelectorAll('.ball');
 
+
+gsap.fromTo(balls, 1, {
+  scale: 0,
+  opacity: 0,
+}, {
+  scale: 1.2,
+  opacity: 1,
+  stagger: .2,
+  repeat: -1,
+  yoyo: true
+});
